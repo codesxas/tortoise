b28046cc8b106fa959c599f04f1798f21afca6b2
codesxas <39775875+codesxas@users.noreply.github.com>
2022-06-26 19:35:56 +0530

    formatted the code

--- src/template/components/Header.tsx
+++ src/template/components/Header.tsx
@@ -4,7 +4,9 @@ function Header() {
   return (
     <div className="header">
       <h1 className="title">Type the Alphabet</h1>
-      <p className="desc">Typing game to see how fast you type. Timer starts when you do. :)</p>
+      <p className="desc">
+        Typing game to see how fast you type. Timer starts when you do. :)
+      </p>
     </div>
   );
 }
